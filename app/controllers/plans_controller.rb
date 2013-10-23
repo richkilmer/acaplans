d34f349dfdea50ae4a0d5c53be03fe848475d78e
Ben Wilson <ben.wilson@livingsocial.com>
2013-10-23 12:45:39 -0400

    initial zip code filter

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,14 @@
 class PlansController < ApplicationController
+  def index
+    if params[:zip_code]
+      @plans = filtered_plans
+    end
+  end
+
+  private
+
+  def filtered_plans
+    plans = Plans.where(fips: Zip.where(code: params[:zip_code]).first)
+    plans.where(plan_type: params[:plan_type]) if params[:plan_type]
+  end
 end
